459beb7ed52391af9714be1a38caba007d841865
agray <agray@pch.com>
2018-11-14 10:23:10 -0500

    debug

--- main.c
+++ main.c
@@ -35,17 +35,21 @@ int main(int argc, char ** argv) {
         return 1;
     }
 
+    int8_t width = 15;
+    int8_t height = 15;
+    int16_t minefield_size = width * height;
+
     /* Dimensions, initial cursor placement, mines, flags */
-    p_grid->width = 15;
-    p_grid->height = 15;
-    p_grid->y_offset = ((LINES - 1) / 2) - (3 * p_grid->height / 4);
-    p_grid->x_offset = ((COLS - 1) / 2) - p_grid->width;
+    p_grid->width = width;
+    p_grid->height = height;
+    p_grid->y_offset = ((LINES - 1) / 2) - (3 * height / 4);
+    p_grid->x_offset = ((COLS - 1) / 2) - width; 
     p_grid->mines = 0;
     p_grid->flags = 0;
-    p_grid->cursor = (p_grid->width / 2) - 1 + (p_grid->width * p_grid->height) / 2;
+    p_grid->cursor = (minefield_size / 2);
 
     /* Create minefield */
-    int8_t * minefield = (int8_t *)malloc(p_grid->width * p_grid->height * sizeof(int8_t));
+    int8_t * minefield = (int8_t *)malloc(minefield_size * sizeof(int8_t));
     if (minefield == NULL) {
         endwin();
         return 1;
@@ -54,34 +58,22 @@ int main(int argc, char ** argv) {
 
     /* Fill minefield with mines and adjacent-to-mine values */
     initialize_minefield(p_grid);
-    int8_t other_stuff = 0;
-    for (int16_t i; i < p_grid->width * p_grid->height; ++i) {
-        if (p_grid->minefield[i] != MINE && p_grid->minefield[i] > 0) {
-            other_stuff = 1;
-        }
-    }
-    if (!other_stuff) {
-        endwin();
-        printf("sad\n");
-        return 1;
-    }
-
 
     /* Render menu */
-    render_menu(p_grid);
+    //render_menu(p_grid);
 
     /* Render outlines */
-    render_outlines(p_grid);
+    //render_outlines(p_grid);
 
     /* Render minefield */
     render_minefield(p_grid);
 
     /* Refresh page to show initial render */
-    refresh();
+    //refresh();
 
     /* Gameplay */
-    while (input(p_grid))
-        ;
+    //while (input(p_grid))
+        //;
 
     getch();
     endwin();
--- mines.c
+++ mines.c
@@ -2,29 +2,34 @@
 
 void initialize_minefield(grid * p_grid) {
     srand(time(NULL));
+    int8_t width = p_grid->width;
+    int8_t height = p_grid->height;
+    int16_t minefield_size = width * height;
 
     /* Fill minefield with mines */
     int8_t * p_minefield = p_grid->minefield;
-    for (int16_t i; i < p_grid->width * p_grid->height; ++i) {
-        *p_minefield = ((rand() & 1) & (rand() & 1) & (rand() & 1) & (rand() & 1)) ? MINE : EMPTY;
+    for (int16_t i; i < minefield_size; ++i) {
+        /* Space is default empty, but has 4 chances to become a MINE */
+        *p_minefield = ((rand() & 1) && (rand() & 1) && (rand() & 1) && (rand() & 1)) ? MINE : EMPTY;
+
         if (*p_minefield & MINE) {
             p_grid->mines += 1;
 
             /* Above */
-            if (i - p_grid->width >= 0 && rand() & 1) {
-                *(p_minefield - p_grid->width) = MINE;
+            if (i - p_grid->width >= 0 && (rand() & 1)) {
+                *(p_minefield - width) = MINE;
                 p_grid->mines += 1;
             }
 
             /* To the left */
-            if (i - 1 >= 0 && rand() & 1) {
+            if (i - 1 >= 0 && (rand() & 1)) {
                 *(p_minefield - 1) = MINE;
                 p_grid->mines += 1;
             }
 
             /* Above and to the left */
-            if (i - p_grid->width - 1 >= 0 && rand() & 1) {
-                *(p_minefield - p_grid->width - 1) = MINE;
+            if (i - width - 1 >= 0 && (rand() & 1)) {
+                *(p_minefield - width - 1) = MINE;
                 p_grid->mines += 1;
             }
         }
@@ -32,41 +37,38 @@ void initialize_minefield(grid * p_grid) {
     }
 
     /* Calculate all other spaces */
-    for (int16_t i; i < (p_grid->width * p_grid->height); ++i) {
+    for (int16_t i; i < minefield_size; ++i) {
         if (p_grid->minefield[i] == EMPTY) {
-            mvprintw(1, 1, "THIS SHOULD BE PRINTED ON SCREEN");
-            refresh();
-
             p_grid->minefield[i] = 
-                ((i < p_grid->width) ? 0 : (i % p_grid->width) ? 0 : (p_grid->minefield[i - p_grid->width - 1] == MINE) ? 1 : 0)
-                + ((i < p_grid->width) ? 0 : (p_grid->minefield[i - p_grid->width] == MINE) ? 1 : 0)
-                + ((i < p_grid->width) ? 0 : ((i + 1) % p_grid->width == 0) ? 0 : (p_grid->minefield[i - p_grid->width + 1] == MINE) ? 1 : 0)
-                + ((i % p_grid->width == 0) ? 0 : (p_grid->minefield[i - 1] == MINE) ? 1 : 0)
-                + (((i + 1) % p_grid->width == 0) ? 0 : (p_grid->minefield[i + 1] == MINE) ? 1 : 0)
-                + ((i >= ((p_grid->width * p_grid->height) - p_grid->width)) ? 0 : (i % p_grid->width == 0) ? 0 : (p_grid->minefield[i + p_grid->width -1] == MINE) ? 1 : 0)
-                + ((i >= ((p_grid->width * p_grid->height) - p_grid->width)) ? 0 : (p_grid->minefield[i + p_grid->width] == MINE) ? 1 : 0)
-                + ((i >= ((p_grid->width * p_grid->height) - p_grid->width)) ? 0 : ((i + 1) % p_grid->width == 0) ? 0 : (p_grid->minefield[i + p_grid->width + 1] == MINE) ? 1 : 0);
+                ((i < width) ? 0 : (i % width) ? 0 : (p_grid->minefield[i - width - 1] & MINE) ? 1 : 0)
+                + ((i < width) ? 0 : (p_grid->minefield[i - width] & MINE) ? 1 : 0)
+                + ((i < width) ? 0 : ((i + 1) % width == 0) ? 0 : (p_grid->minefield[i - width + 1] & MINE) ? 1 : 0)
+                + ((i % width == 0) ? 0 : (p_grid->minefield[i - 1] & MINE) ? 1 : 0)
+                + (((i + 1) % width == 0) ? 0 : (p_grid->minefield[i + 1] & MINE) ? 1 : 0)
+                + ((i >= ((width * height) - width)) ? 0 : (i % width == 0) ? 0 : (p_grid->minefield[i + width -1] & MINE) ? 1 : 0)
+                + ((i >= ((width * height) - width)) ? 0 : (p_grid->minefield[i + width] & MINE) ? 1 : 0)
+                + ((i >= ((width * height) - width)) ? 0 : ((i + 1) % width == 0) ? 0 : (p_grid->minefield[i + width + 1] & MINE) ? 1 : 0);
         }
     }
 }
 
 
 void render_minefield(grid * p_grid) {
+    static int16_t p = 0;
+
     int8_t * p_minefield = p_grid->minefield;
-    for (int16_t i = 0; i < p_grid->width * p_grid->height; ++i) {
+    int16_t minefield_size = p_grid->width * p_grid->height;
+
+    for (int16_t i = 0; i < minefield_size; ++i) {
         /* Turn on cursor */
         if (i == p_grid->cursor) {
             attron(COLOR_PAIR(CURSOR));
         }
 
-        if ((*p_minefield & VALUE) > 0) {
-            mvprintw(1, 1, "value geater than 0");
-        }
-
         mvaddch(
             p_grid->y_offset + (i / p_grid->width),
             p_grid->x_offset + (2 * (i % p_grid->width)),
-            (*p_minefield & MINE) ? 'm' : (*p_minefield & VALUE) + '0'
+            (*p_minefield & MINE) ? 'm' : 'a' + (p_grid->minefield[i]) 
         );
 
         attroff(COLOR_PAIR(CURSOR));
