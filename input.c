9683b08ffbe826cc268767a2449e810f6c128e56
Adam Gray <kruschka.kruschka@gmail.com>
2019-01-07 16:52:08 -0500

    add win condition

--- input.c
+++ input.c
@@ -1,5 +1,18 @@
 #include "input.h"
 
+int8_t end_conditions(grid * p_grid) {
+    /* Number of spaces actually uncovered */
+    int16_t actual = 0;
+    for (int16_t i = 0; i < p_grid->width * p_grid->height; ++i) {
+        if (p_grid->minefield[i] & UNCOVERED) {
+            /* Increment actual for each uncovered space */
+            ++actual;
+        }
+    }
+    /* return the comparison between the number of spaces actually uncovered with the number of spaces needed for win */
+    return (actual == (p_grid->width * p_grid->height) - p_grid->mines) ? 1 : 0;
+}
+
 int8_t input(grid * p_grid) {
     int16_t c = getch();
     switch (c) {
@@ -8,6 +21,7 @@ int8_t input(grid * p_grid) {
             return 0;
         /* DIG */
         case 'd':
+            /* If the space was a mine, you lose haha */
             if (p_grid->minefield[p_grid->cursor] & MINE) {
                 /* Unflag */
                 p_grid->minefield[p_grid->cursor] &= ~FLAGGED;
@@ -19,16 +33,15 @@ int8_t input(grid * p_grid) {
                 mvprintw(
                     p_grid->y_offset + p_grid->height + 3,
                     p_grid->x_offset,
-                    "       GAME OVER       "
+                    "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
                 );
                 /* Wait for any input */
                 getch();
                 /* QUIT */
                 return 0;
             }
-            /* Reveal this space and, recursively, all the surrounding spaces that may be empty */
+            /* Otherwise, reveal this space and, recursively, all the surrounding spaces that may be empty */
             reveal(p_grid, p_grid->cursor);
-
             /* Render the now uncovered space with the cursor on it */
             render_mine(p_grid, p_grid->cursor, 1);
             break;
@@ -40,7 +53,6 @@ int8_t input(grid * p_grid) {
                 p_grid->flags += 1;
                 render_mine(p_grid, p_grid->cursor, 0);
                 render_menu(p_grid);
-
             /* Only unflag spaces that are covered and flagged */
             } else if (!(p_grid->minefield[p_grid->cursor] & UNCOVERED) && (p_grid->minefield[p_grid->cursor] & FLAGGED)) {
                 p_grid->minefield[p_grid->cursor] &= ~FLAGGED;
@@ -48,7 +60,6 @@ int8_t input(grid * p_grid) {
                 render_mine(p_grid, p_grid->cursor, 1);
                 render_menu(p_grid);
             }
-
             break;
         /* MOVEMENT */
         case 'h':
@@ -82,6 +93,17 @@ int8_t input(grid * p_grid) {
         default:
             break;
     }
-
+    /* Check for end conditions */
+    if (end_conditions(p_grid)) {
+        /* Wait for any input */
+        mvprintw(
+                p_grid->y_offset + p_grid->height + 3,
+                p_grid->x_offset,
+                "WINNER WINNER WINNER WINNER"
+                );
+        getch();
+        /* QUIT */
+        return 0;
+    }
     return 1;
 }
